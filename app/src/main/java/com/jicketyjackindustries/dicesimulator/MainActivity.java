3bce8059e8e4d4af3a7463245493864eead2dc4b
Steve Haskell <steven.haskell.is@gmail.com>
2019-01-29 12:37:32 -0800

    shows up to 6 dice rolls and their sum. Results are displayed but not which dice was rolled, you just need to know which button you pushed with this one

--- app/src/main/java/com/jicketyjackindustries/dicesimulator/MainActivity.java
+++ app/src/main/java/com/jicketyjackindustries/dicesimulator/MainActivity.java
@@ -4,21 +4,67 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView t;
-
+    private List<Integer> diceResults;
+    private List<String> diceNamesUsed;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         t = findViewById(R.id.textViewResField);
+        diceResults = new LinkedList<Integer>();
+        diceNamesUsed = new LinkedList<String>();
     }
 
+    public void updateResultField()
+    {
+        String textToDisplay="error text not assigned";
+        int maxNumResults=6;
+        if(diceResults.isEmpty() || diceNamesUsed.isEmpty()) {
+            textToDisplay = "";
+        }else if(diceResults.size()!=diceNamesUsed.size()){
+            textToDisplay = "error: sizes different";
+        }else if(diceResults.size()==1){
+            textToDisplay = diceResults.get(0).toString();
+        }
+        else {
+            int lastIndex;
+            if(diceResults.size()>maxNumResults) lastIndex=maxNumResults-1;
+            else lastIndex = diceResults.size()-1;
+
+            Integer sum = 0;
+            textToDisplay = "";
+            for (int i = lastIndex; i >= 0; i--) {
+                sum = sum+diceResults.get(i);
+                if(i!=lastIndex) textToDisplay = diceResults.get(i) + "+" + textToDisplay;
+                else textToDisplay = diceResults.get(i) +textToDisplay;
+
+            }
+            textToDisplay = textToDisplay+"="+sum.toString();
+        }
+
+        if(diceResults.size()>maxNumResults){
+            String message = "Tap Results to Clear";
+            Toast myToast = Toast.makeText(getApplicationContext(),message, Toast.LENGTH_LONG);
+            myToast.show();
+        }
+        t.setText(textToDisplay);
+    }
+
+    public void clearLists(View v){
+        diceResults.clear();
+        diceNamesUsed.clear();
+        updateResultField();
+    }
     public void setResultField(int res, String typeOfDice){
         String s = Integer.toString(res);
         t.setText(typeOfDice + " :  "+ s);
@@ -48,8 +94,10 @@ public class MainActivity extends AppCompatActivity {
             diceName = "N/A";
         }
         int res = getRandomBetween(1,typeOfDice);
-
-        setResultField(res,diceName);
+        diceResults.add(res);
+        diceNamesUsed.add(diceName);
+        updateResultField();
+        //setResultField(res,diceName);
     }
     public void onD4Roll(View v){
         onRoll(4);
